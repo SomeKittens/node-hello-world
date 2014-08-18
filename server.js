9b5bf1efad1e08b95dc32355754464eecf97fc56
SomeKittens <rkoutnik@gmail.com>
2014-08-18 13:13:28 -0700

    whitespace

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
+
 var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(80);
+
 console.log('Server running at http://127.0.0.1:80/');
+
 setInterval(function() {
   console.log('Glorious Pants', Math.random());
 }, 3000);
